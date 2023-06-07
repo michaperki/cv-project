5f293a4d74b681ecd6b387d11b93657f1eba78d8
Michael Perkins <mperkins1995@gmail.com>
2023-06-07 04:41:10 -0400

    added type and placeholder props to Input component

--- cv-project/src/components/CVForm/Personal.js
+++ cv-project/src/components/CVForm/Personal.js
@@ -2,11 +2,22 @@ import React from "react";
 import Input from "../Utils/Input";
 
 const Personal = ({ personalInfo, onChange }) => {
-
   return (
     <>
-      <Input name="name" value={personalInfo.name} onChange={(e) => onChange(e)} />
-      <Input name="email" value={personalInfo.email} onChange={(e) => onChange(e)} />
+      <Input
+        name="name"
+        value={personalInfo.name}
+        onChange={(e) => onChange(e)}
+        type="text"
+        placeholder="name"
+      />
+      <Input
+        name="email"
+        value={personalInfo.email}
+        onChange={(e) => onChange(e)}
+        type="text"
+        placeholder="email"
+      />
     </>
   );
 };
--- cv-project/src/components/Utils/Input.js
+++ cv-project/src/components/Utils/Input.js
@@ -1,8 +1,16 @@
 import React from "react";
 import styled from "styled-components";
 
-const Input = ({ name, value, onChange }) => {
-  return <InputWrapper name={name} value={value} onChange={onChange} />;
+const Input = ({ name, value, onChange, type, placeholder }) => {
+  return (
+    <InputWrapper
+      name={name}
+      value={value}
+      onChange={onChange}
+      type={type}
+      placeholder={placeholder}
+    />
+  );
 };
 
 const InputWrapper = styled.input`
